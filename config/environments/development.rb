1b8cef68ff00a611873da1a72d86a5c42514890e
sukhi <surekhagagaliya@gmail.com>
2019-12-28 14:00:12 +0530

    Set env variable on development.rb file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,12 +61,12 @@ Rails.application.configure do
   # config.action_mailer.default_url_options = { host: host }
 
   # SMTP settings for gmail
-  # config.action_mailer.smtp_settings = {
-  #   address:              "smtp.gmail.com",
-  #   port:                 587,
-  #   user_name:            '',
-  #   password:             '',
-  #   authentication:       "plain",
-  #   enable_starttls_auto: true
-  # }
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       "plain",
+    enable_starttls_auto: true
+  }
 end
