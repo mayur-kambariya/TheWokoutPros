ba768fb9e64794b1802cd6d4b6d4b332b5a3792f
sukhi <surekhagagaliya@gmail.com>
2019-12-28 14:46:58 +0530

    Change mail setting for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'https://www.theworkoutpros.com/' }
+  config.action_mailer.delivery_method = :smtp
+
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
