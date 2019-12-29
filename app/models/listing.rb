bb0a2a2801b7a580d081fb3a9576a85a3098c7b9
Mayur Kambariya <mayurkambariya@gmail.com>
2019-12-29 23:11:30 +0530

    optional true remove from listing api

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,9 @@
 class Listing < ApplicationRecord
   ## RelationShip
   belongs_to :user
-  belongs_to :custom_field_option_title, optional: true
-  belongs_to :category, optional: true
-  belongs_to :listing_shape,optional: true
+  belongs_to :custom_field_option_title
+  belongs_to :category
+  belongs_to :listing_shape
   has_one :location, dependent: :destroy
   accepts_nested_attributes_for :location
   has_many_attached :listing_images
