607dc9674e089784b519931a6d5c24ab2440c353
sukhi <surekhagagaliya@gmail.com>
2020-01-01 22:08:11 +0530

    Created_at and updated_at in category

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,4 +1,4 @@
 class CategorySerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :community_id, :sort_priority, :url
+  attributes :id, :community_id, :sort_priority, :url, :created_at, :updated_at
 end
